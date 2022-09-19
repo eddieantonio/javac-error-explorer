5c1cade567f276086a4ea5dc2660b6c9ec2d3525
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-09-19 17:19:16 +0100

    [wip]: parse everything except placeholder type comments

--- parse-compile-properties.py
+++ parse-compile-properties.py
@@ -1,5 +1,6 @@
+import inspect
+import re
 from dataclasses import dataclass
-from enum import Enum, auto
 from pathlib import Path
 from typing import Sequence
 
@@ -13,87 +14,178 @@ properties_text = properties_file.read_text(encoding="UTF-8")
 @dataclass(frozen=True)
 class Placeholder:
     index: int
+    type_: str
+    comment: str
 
     def __str__(self):
+        # Didn't use f-string because the {{{_}}} gets confusing.
         return "{" + int(self.index) + "}"
 
 
 @dataclass
 class Message:
-    raw_text: str
-    _components: Sequence[Placeholder | str]
+    name: str
+    components: Sequence[Placeholder | str]
 
     def __str__(self):
         return "".join(str(item) for item in self.components)
 
 
 class ParseError(Exception):
-    def __init__(self, line_no: int, message: str):
+    def __init__(self, *, line_no: int, line: str, production: str, message: str):
         self.line_no = line_no
         self.message = message
+        self.production = production
+        self.message = message
+        self.line = line
 
     def __str__(self):
-        return f"line {self.line_no}: {self.message}"
+        filename = properties_file.name
+        return (
+            f"{filename}:{self.line_no}: {self.message}\n"
+            "\n"
+            f"  {self.line_no} | {self.line}"
+            "\n"
+            f"(while in {self.production})"
+        )
 
 
 # Parse some messages!
+class Parser:
+    def __init__(self, lines: Sequence[str]):
+        self.lines = enumerate(lines, start=1)
+        self.line_no, self.raw_line = next(self.lines)
+        self.parsing = True
+        self.messages: list[Message] = []
+        self.current_comment_lines: list[str] = []
 
-messages: list[Message] = []
-comment_buffer: list[str] = []
-current_value = []
+    @property
+    def line(self) -> str:
+        return self.raw_line.rstrip()
 
-parsing = True
+    def parse(self):
+        while True:
+            self.item()
+            try:
+                self.next_line()
+            except StopIteration:
+                break
+
+    def next_line(self):
+        self.line_no, self.raw_line = next(self.lines)
+
+    def item(self):
+        if self.line.startswith("#"):
+            self.comment()
+        elif self.line == "":
+            # Empty line. Clear the current comment.
+            self.current_comment_lines = []
+        elif self.line[0].isalpha():
+            # A message without an annotation
+            self.message()
+        else:
+            self.parse_error()
+
+    def comment(self):
+        """
+        Parsing either an "annotation":
+
+        # 0: type
+
+        or a regular comment like
+
+        # hello
+        """
+        self.current_comment_lines.append(self.line)
+
+    def message(self):
+        """
+        Parse a property like:
+
+            compiler.error.syntax=syntax error
+        """
+        name, sep, rest = self.line.partition("=")
+
+        if sep != "=":
+            raise self.parse_error("expected `property=` here")
+
+        # A few names have spaces around the property name.
+        name = name.strip()
+
+        if rest.endswith("\\"):
+            self.next_line()
+            value = self.value()
+        else:
+            value = [rest]
+
+        self.messages.append(Message(name=name, components=self.make_components(value)))
+        self.current_comment_lines = []
+
+    def value(self) -> str:
+        """
+        Parsing a property value, i.e., the part after equals
+
+            conatiner {0} is not applicable to element {1}
+        """
+
+        value_chunks = []
+
+        has_continuation = True
+        while has_continuation:
+            chunk = self.line.lstrip().removesuffix("\\")
+            value_chunks.append(chunk)
+            has_continuation = self.line.endswith("\\")
+            try:
+                self.next_line()
+            except StopIteration:
+                break
+        return "".join(value_chunks)
+
+    def make_components(self, message_text: str) -> Sequence[str | Placeholder]:
+        """
+        Uh.... hard to explain
+        """
+
+        types = parse_annotation(self.current_comment_lines)
+        # TODO: this entire function
+        return [message_text]
+
+    def parse_error(self, message=None):
+        """
+        Helper to create a nice parse error.
+        """
+        name = inspect.stack()[1].function
+        return ParseError(
+            line_no=self.line_no,
+            line=self.line,
+            production=name,
+            message=message or "Parse error",
+        )
 
 
-class State:
-    LOOKING_FOR_ITEM = "looking for any item"
-    PARSING_VALUE = "parsing a value"
-    PARSING_COMMENT = "parsing a comment"
+def parse_annotation(lines: Sequence[str]) -> dict[int, Placeholder]:
+    """
+    Parse annotations. Annotations might have comments.
+    """
+
+    if len(lines) == 0:
+        # No placeholder type annotations to be found!
+        return {}
+
+    # Only the last line has annotations
+    annotation_line = lines[-1]
+
+    if ":" not in annotation_line:
+        print("Warning: not interpreting this as an annotation:", lines)
+        return {}
+
+    # TODO: do the actual parsing
+    print(annotation_line)
+
+    return {}
 
 
-state = State.LOOKING_FOR_ITEM
-
-for line_no, line in enumerate(properties_text.splitlines(), start=1):
-    line = line.rstrip()
-
-    match state:
-        case State.LOOKING_FOR_ITEM | State.PARSING_COMMENT:
-            if line.startswith("#"):
-                comment_buffer.append(line)
-                state = State.PARSING_COMMENT
-            elif line == "":
-                comment_buffer = []
-            elif line[0].isalpha():
-                # Found a property, like:
-                #
-                #  compiler.error.syntax=syntax error
-                name, sep, rest = line.partition("=")
-
-                if name.strip() != name:
-                    raise ParseError(
-                        line_no, f"found spaces around property name: {line}"
-                    )
-                if sep != "=":
-                    raise ParseError(
-                        line_no, f"Expected property= but instead got: {line}"
-                    )
-
-                if rest.endswith("\\"):
-                    state = State.PARSING_VALUE
-                else:
-                    state = State.LOOKING_FOR_ITEM
-                    raise NotImplementedError("one line message")
-            else:
-                raise ParseError(line_no, f"Unexpected line: {line!r}")
-        case State.PARSING_VALUE:
-            if line == "":
-                state = State.LOOKING_FOR_ITEM
-            elif line[0].isspace():
-                assert not line[-1].isspace()
-                current_value.append(line)
-            else:
-                current_value.append(line)
-        case _:
-            raise ParseError(line_no, f"Unhandled state: {state}")
-
-print("done")
+p = Parser(properties_text.splitlines())
+p.parse()
+for message in p.messages:
+    print(message.name, message)
