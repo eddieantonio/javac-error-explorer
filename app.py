081e0feca56adb613e3b0bd3b281cc4054f7ca8b
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-09-21 11:02:40 +0100

    change: futz around with message detail

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from rich import print
 
 import constants
-from data_layer import messages
+from data_layer import message_list, messages
 from javac_compiler_properties_parser import Placeholder
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ def index():
     return render_template("index.html", messages=messages().values())
 
 
-@app.route("/rate/<message_id>")
+@app.route("/message/<message_id>")
 def message_detail(message_id: str):
 
     # Get the message
@@ -64,6 +64,37 @@ def message_detail(message_id: str):
         components=components,
         stack_overflow_search=stack_overflow_search,
     )
+
+
+@app.route("/next-message/<message_id>")
+def next_message(message_id: str):
+    return seek_message(message_id, diff=+1, default_index=0)
+
+
+@app.route("/previous-message/<message_id>")
+def previous_message(message_id: str):
+    return seek_message(message_id, diff=-1, default_index=-1)
+
+
+def seek_message(message_id: str, *, diff: int, default_index: int):
+    """
+    Get a message offset from the given message ID.
+    """
+    current = messages().get(message_id)
+    if current is None:
+        return render_template("message-not-found.html", message_id=message_id), 404
+
+    # Uh... ignore the fact that this code is awful.
+    all_messages = message_list()
+    current_index = all_messages.index(current)
+    next_index = current_index + diff
+    try:
+        next_message = all_messages[next_index]
+    except IndexError:
+        # Wrap around
+        next_message = all_messages[default_index]
+
+    return redirect(url_for("message_detail", message_id=next_message.name))
 
 
 # Place all globals in the Jinja environment
